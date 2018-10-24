b26ddb25fd43c7394b4682f5dc7862782d53c98b
Emaga Alain <alaincesar@hotmail.fr>
2018-10-24 16:37:52 +0100

    Project completed

--- client/main.js
+++ client/main.js
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
-import {Players} from './../imports/api/players';
+import {Players, calculatePlayersPosition} from './../imports/api/players';
 import App from './../imports/ui/App';
 
 Meteor.startup(() => {
 
   Tracker.autorun(() => {
-
     let players = Players.find({}, {
       sort: {
         score: -1
       }
     }).fetch();
+    let positionedPlayers = calculatePlayersPosition(players);
     let title = 'Score Keep';
-
-    ReactDOM.render(<App title={title} players={players} />, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={positionedPlayers} />, document.getElementById('app'));
   });
 });
--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FlipMove from 'react-flip-move';
 
 import {Players} from './../api/players';
 import Player from './Player';
@@ -24,7 +25,9 @@ export default class PlayerList extends React.Component {
   render(){
     return(
       <div>
-        {this.getPlayers(this.props.players)}
+        <FlipMove maintainContainerHeight={true}>
+          {this.getPlayers(this.props.players)}
+        </FlipMove>
       </div>
     );
   }
